733c5f91d3fc246a3ae3d06f4d9fb33882bcdc90
Jonathan Curran <joncfoo@gmail.com>
2017-11-03 16:51:39 -0600

    Do not use Future.reject (unnecessary)

--- examples/auth-tokens-program.js
+++ examples/auth-tokens-program.js
@@ -1,6 +1,7 @@
 const daggy = require('daggy')
 const {Program, makeInstructions} = require('../src/index')
 
+// Result error success
 const Result = daggy.taggedSum('Result', {
     Error: ['error'],
     Ok: ['value'],
@@ -11,6 +12,7 @@ const UIAction = daggy.taggedSum('UIAction', {
     Delete: ['tokenID'],
     Create: [],
     Exit: [],
+    Unknown: ['helpMessage'],
 })
 
 const Token = daggy.tagged('Token', ['id', 'token'])
@@ -20,13 +22,13 @@ const Instruction = makeInstructions('Instruction', {
     // ==== external API ====
     // ======================
 
-    // ListTokens :: Program (Result [Token])
+    // ListTokens :: Program (Result String [Token])
     ListTokens: [],
 
-    // DeleteToken :: Int -> Program ()
+    // DeleteToken :: Int -> Program (Result String {})
     DeleteToken: ['tokenID'],
 
-    // CreateToken :: String -> Program Token
+    // CreateToken :: String -> Program (Result String Token)
     CreateToken: ['tokenName'],
 
     // ====================
@@ -36,7 +38,7 @@ const Instruction = makeInstructions('Instruction', {
     // NotifyError :: String -> Program ()
     NotifyError: ['error'],
 
-    // AwaitUIAction :: Program UIAction
+    // AwaitUIAction :: Program (Result UIAction)
     AwaitUIAction: [],
 
     // ConfirmDeletion :: Int -> Program ()
@@ -53,7 +55,7 @@ const Instruction = makeInstructions('Instruction', {
 })
 
 const program = Program.do(function *() {
-    // tokensResult :: Result [Token]
+    // tokensResult :: Result String [Token]
     const tokensResult = yield Instruction.ListTokens
 
     // upon failure notify of error
@@ -62,36 +64,57 @@ const program = Program.do(function *() {
         return
     }
 
+    // loop forever, waiting for UIActions
     while (1) {
-        // actionResult :: Result UIAction
+
+        // actionResult :: Result String UIAction
         const actionResult = yield Instruction.AwaitUIAction
 
+        // if there was an error, simply display and wait for input again
         if (Result.Error.is(actionResult)) {
             yield Instruction.NotifyError(actionResult.error)
-            break
+            continue
         }
 
+        // extract UIAction
         const action = actionResult.value
 
+        // asked to quit
         if (UIAction.Exit.is(action)) {
             break
         }
 
+        // unsure what user wanted so try again
+        if (UIAction.Unknown.is(action)) {
+            yield Instruction.NotifyError(action.helpMessage)
+            continue
+        }
+
+        // delete a token
         if (UIAction.Delete.is(action)) {
             const tokenID = action.tokenID
+
+            // ask for confirmation
             const confirmed = yield Instruction.ConfirmDeletion(tokenID)
 
             if (confirmed) {
                 const deleteResult = yield Instruction.DeleteToken(tokenID)
+
+                // if there was an error simply notify the user
                 if (Result.Error.is(deleteResult)) {
                     yield Instruction.NotifyError(deleteResult.error)
                 }
             }
         }
 
+        // create token
         if (UIAction.Create.is(action)) {
+            // ask for token name
             const tokenName = yield Instruction.PromptNewTokenName
+
             const tokenResult = yield Instruction.CreateToken(tokenName)
+
+            // notify token creation result
             if (Result.Error.is(tokenResult)) {
                 yield Instruction.NotifyError(tokenResult.error)
             } else {
@@ -99,6 +122,7 @@ const program = Program.do(function *() {
             }
         }
 
+        // display all tokens
         if (UIAction.Show.is(action)) {
             yield Instruction.DisplayAllTokens
         }
--- examples/auth-tokens-terminal.js
+++ examples/auth-tokens-terminal.js
@@ -43,12 +43,12 @@ Store.prototype.createToken = function(tokenName) {
     return Future.after(1000, Result.Ok(token))
 }
 
-Store.removeToken = function(tokenID) {
+Store.prototype.removeToken = function(tokenID) {
     const i = DataStore.tokens.findIndex(t => t.id === tokenID)
     if (i < 0) {
         return Future.of(Result.Error(`Invalid token ID: ${tokenID}`))
     } else {
-        this.tokens = this.tokens.filter(t => t.id !== tokenId)
+        this.tokens = this.tokens.filter(t => t.id !== tokenID)
         return Future.of(Result.Ok({}))
     }
 }
@@ -67,7 +67,10 @@ const effectsInterpreter = {
         Future.of(value),
 
     NotifyError: (error) =>
-        Future.reject(error),
+        Future((_, resolve) => {
+            console.error('Error:', error)
+            resolve({})
+        }),
 
     ListTokens: () =>
         DataStore.fetchTokens(),
@@ -78,17 +81,15 @@ const effectsInterpreter = {
     CreateToken: (tokenName) =>
         DataStore.createToken(tokenName),
 
-    AwaitUIAction: () => {
-        return new Future((reject, resolve) => {
-            const prompt = `
-What would you like to do?
+    AwaitUIAction: () =>
+        Future((_, resolve) => {
+            const prompt = `What would you like to do?
 S: show all tokens
 C: create a token
 D: delete a token
 Q: quit
 action #> `
             const resolveOk = value => resolve(Result.Ok(value))
-            const resolveError = err => resolve(Result.Error(err))
 
             ReadLine.question(prompt, input => {
                 switch (input.toLowerCase()) {
@@ -101,7 +102,7 @@ action #> `
                 case 'd':
                     ReadLine.question('Which token id? #> ', id => {
                         if (Number.isNaN(id))
-                            resolveError(`${id} is not a valid ID`)
+                            resolveOk(UIAction.Unknown(`${id} is not a valid ID`))
                         else
                             resolveOk(UIAction.Delete(Number(id)))
                     })
@@ -110,34 +111,31 @@ action #> `
                     resolveOk(UIAction.Exit)
                     break
                 default:
-                    resolveError(`${input} is not a valid action`)
+                    resolveOk(UIAction.Unknown(`${input} is not a valid action`))
                     break
                 }
             })
-        })
-    },
+        }),
 
     DisplayAllTokens: () => {
         DataStore.tokens.forEach(t => console.log(t.toString()))
         return Future.of({})
     },
 
-    ConfirmDeletion: (tokenID) => {
-        return new Future((reject, resolve) => {
+    ConfirmDeletion: (tokenID) =>
+        Future((_, resolve) => {
             const prompt = `Are you sure you want to delete token ${tokenID}? [Y/N] #> `
             ReadLine.question(prompt, sure => {
                 resolve(sure.toLowerCase() === 'y')
             })
-        })
-    },
+        }),
 
-    PromptNewTokenName: () => {
-        return new Future((reject, resolve) => {
+    PromptNewTokenName: () =>
+        Future((_, resolve) => {
             ReadLine.question('New token name? #> ', name => {
                 resolve(name)
             })
-        })
-    },
+        }),
 
     DisplayNewToken: (token) => {
         console.log('New token created:', token.toString())
@@ -146,8 +144,13 @@ action #> `
 }
 
 program.interpretM(effectsInterpreter)
-    .fold(Result.Error, Result.Ok)
-    .value(value => {
-        console.log('Program returned:', value.toString())
-        ReadLine.close()
-    })
+    .fork(
+        error => {
+            console.error('Program error:', error)
+            ReadLine.close()
+        },
+        value => {
+            console.log('Program returned:', value.toString())
+            ReadLine.close()
+        }
+    )
